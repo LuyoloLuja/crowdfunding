d5cef4660c0f2ee060d9453d8531ef3a1b390109
LuyoloLuja <luyolom19@gmail.com>
2021-03-01 18:52:27 +0200

    added calculations to my pledge buttons

--- js/main.js
+++ js/main.js
@@ -1,40 +1,27 @@
 const makePledgeBtn = document.querySelector('.make-pledge');
 const modalBackground = document.querySelector('.bg-modal');
+const modalBackgroundTwo = document.querySelector('.pledge-successful');
 const closeModal = document.querySelector('.close-btn');
+const closeModalTwo = document.querySelector('.close-btn-two');
 
 const bambooEdition = document.querySelector('.bamboo-stand');
 const bambooEdtionBtn = document.querySelector('.bamboo-edition-btn');
-const bambooActiveStatus = document.querySelector('.pledgeTwo')
-let bambooInputElement = document.querySelector('.bambooInputElement');
-const bambooBtnElement = document.querySelector('.bambooBtnElement');
+const bambooActiveStatus = document.querySelector('.pledgeTwo');
 
-const backedUsers = document.querySelector('.backed-users');
+const blackEditionButton = document.querySelector('.black-edition-btn');
+
+const pledgedAmount = document.querySelector('.backed-users');
 const activeStatus = document.querySelectorAll('.active');
-const successElement = document.querySelector('.success-message');
 
-const navButton = document.querySelector('.burger');
-const mobileNav = document.querySelector('.display-none');
-const closeNav = document.querySelector('.closeNav');
+const successElement = document.querySelector('.pledge-successful');
 
-navButton.addEventListener('click', () => { 
-    mobileNav.style.display = 'block';
-    navButton.style.display = 'none';
-    closeNav.style.display = 'block';
-})
-closeNav.addEventListener('click', () => { 
-    closeNav.style.display = 'none';
-    mobileNav.style.display = 'none';
-    navButton.style.display = 'block'
-})
-// window.addEventListener('click', (clicking) => {
-//     if (clicking.target === modalBackground) {
-//         modalBackground.style.display = 'none';
-//     }
-// })
-
-closeModal.addEventListener('click', () => {
+function closeModalFunction() {
     modalBackground.style.display = 'none';
-})
+    modalBackgroundTwo.style.display = 'none';
+}
+closeModal.addEventListener('click', closeModalFunction)
+closeModalTwo.addEventListener('click', closeModalFunction)
+
 window.addEventListener('click', (clicking) => {
     if (clicking.target === modalBackground) {
         modalBackground.style.display = 'none';
@@ -49,7 +36,7 @@ function makePledge() {
 
 function selectedEdition() {
     modalBackground.style.display = 'block';
-    bambooActiveStatus.classList.add('active');
+    // bambooActiveStatus.classList.add('active');
 
     // bambooEdition.classList.add('.selected-edition-style');
     // bambooEdition.forEach(edition => {
@@ -62,25 +49,38 @@ function selectedEdition() {
 
 }
 bambooEdtionBtn.addEventListener('click', selectedEdition);
+blackEditionButton.addEventListener('click', selectedEdition);
 
-let bambooDonations = [];
-function calculateBamboo() {
-    const bambooInputValue = parseFloat(bambooInputElement.value);
+const wrapper = document.getElementById('wrapper');
+// const pledgedAmount = document.querySelector('.backed-users');
+const backersElement = document.querySelector('.number-of-backers');
 
-    if(bambooInputValue < 25) return alert('Pledge must be at least $25.');
+wrapper.addEventListener('click', calculateBamboo);
 
-    bambooDonations.push(bambooInputValue);
+let donationsMade = [];
 
-    // get total of donations
-    const totalDonations = bambooDonations.reduce((a, b) =>  a += b, 0);
-    bambooInputValue.value = "";
-     
-    console.log(totalDonations + 'total donated');
+function calculateBamboo(event) {
+    /* Here you can access to the event argument,
+      which contains target: the clicked element*/
+    const el = event.target;
 
-    // need a way to add totalDonations from all editions
-    backedUsers.textContent = totalDonations;
-    
-    return totalDonations;
-    
+    if (el.nodeName !== 'BUTTON' || !el.classList.contains('submitBtn')) {
+        return;
+    }
+
+    // Get target input from button's data-attr
+    const targetInput = el.dataset.input;
+    const inputElement = document.querySelector(`input[data-input="${targetInput}"]`);
+
+    // Continue with the code you had...
+    const inputValue = parseFloat(inputElement.value) || 0;
+
+    if (inputValue < 25 || inputValue === '') return alert('Pledge must be at least $25.');
+
+    donationsMade.push(inputValue);
+    const donationsTotal = donationsMade.reduce((a, b) => a += b);
+    pledgedAmount.textContent = `$${donationsTotal}`;
+    backersElement.textContent = donationsMade.length;
+
+    return donationsTotal;
 }
-bambooBtnElement.addEventListener('click', calculateBamboo);
